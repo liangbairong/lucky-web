ed9fcee18659134b5d0fce2445a20d6abdbb7af9
lll <2222772@163.com>
2023-05-10 17:31:06 +0800

    feat(all): 兼容获取高度

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,17 @@ import { render } from './lucky-web';
 import App from './develop/App';
 import './develop/index.css';
 
+const w = window.screen.width || document.body.clientWidth;
+const h = window.screen.height || document.body.clientHeight;
+// const app = render(<App />, document.getElementById('canvas')!, { devicePixelRatio: 1, width: 750, height: h / (w / 750) });
 
-const app = render(<App />, document.getElementById('canvas')!, { devicePixelRatio: 1, width: 750, height: document.body.clientHeight / (document.body.clientWidth / 750) });
 
-
-// const app = render(<App />, document.getElementById('canvas')!, {  });
-window.addEventListener('resize', () => {
-  requestAnimationFrame(() => {
-    app.update({  width: 750, height: document.body.clientHeight / (document.body.clientWidth / 750) });
-  });
-});
+const app = render(<App />, document.getElementById('canvas')!, {  });
+// window.addEventListener('resize', () => {
+//   requestAnimationFrame(() => {
+//     app.update({  width: 750, height: document.body.clientHeight / (document.body.clientWidth / 750) });
+//   });
+// });
 
 
 // @ts-ignore
