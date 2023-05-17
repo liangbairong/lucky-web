65678332b4d6d3b1365cfa61e94b2e57b12df812
lll <17688946637@163.com>
2023-05-17 16:28:07 +0800

    feat(all): 兼容pc单位

--- src/lucky-web/web/index.ts
+++ src/lucky-web/web/index.ts
@@ -1,31 +1,32 @@
-import { adapter } from '../core/utils';
-import { RevasCanvas } from '../core/Canvas';
+import {adapter} from '../core/utils';
+import {RevasCanvas} from '../core/Canvas';
 
 export * from './render';
 
 
-export function lpx(num:number){
-    return num * (window.screen.width / 750)
+export function lpx(num: number) {
+    const w = document.documentElement.clientWidth || document.body.clientWidth || window.screen.width
+    return num * (w / 750)
 }
 
 
-adapter.createOffscreenCanvas = (width: number, height: number,deviceRatio:number) => {
-  const dom = document.createElement('canvas');
-  const scale =deviceRatio|| window.devicePixelRatio;  //TODO:得修改为从外面传参
-  dom.width = width * scale;
-  dom.height = height * scale;
-  const context = dom.getContext('2d')!;
-  const canvas = new RevasCanvas(context);
-  canvas.transform.scale(scale, scale);
-  return canvas;
+adapter.createOffscreenCanvas = (width: number, height: number, deviceRatio: number) => {
+    const dom = document.createElement('canvas');
+    const scale = deviceRatio || window.devicePixelRatio;  //TODO:得修改为从外面传参
+    dom.width = width * scale;
+    dom.height = height * scale;
+    const context = dom.getContext('2d')!;
+    const canvas = new RevasCanvas(context);
+    canvas.transform.scale(scale, scale);
+    return canvas;
 };
 
-adapter.resetOffscreenCanvas = (prev: RevasCanvas, width: number, height: number,deviceRatio:number) => {
-  const { context, element } = prev;
-  const scale =deviceRatio || window.devicePixelRatio; //TODO:得修改为从外面传参
-  element.width = width * scale;
-  element.height = height * scale;
-  const canvas = new RevasCanvas(context);
-  canvas.transform.scale(scale, scale);
-  return canvas;
+adapter.resetOffscreenCanvas = (prev: RevasCanvas, width: number, height: number, deviceRatio: number) => {
+    const {context, element} = prev;
+    const scale = deviceRatio || window.devicePixelRatio; //TODO:得修改为从外面传参
+    element.width = width * scale;
+    element.height = height * scale;
+    const canvas = new RevasCanvas(context);
+    canvas.transform.scale(scale, scale);
+    return canvas;
 };
