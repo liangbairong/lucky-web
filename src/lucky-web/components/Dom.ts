a538d2474194547ea5baf2c71be5fbc70c9e3041
lll <17688946637@163.com>
2023-05-25 14:30:10 +0800

    feat(all):Dom组件修改属性

--- src/lucky-web/components/Dom.ts
+++ src/lucky-web/components/Dom.ts
@@ -4,8 +4,8 @@ import {useCallback, useEffect, useRef, useState} from "react";
 import {RevasCanvas} from "../core/Canvas";
 
 
-export type IInput = {
-    html?: React.ReactNode
+export type IDom = {
+    html?: string
 } & BaseProps;
 
 function hasScrollContent(node: any, func: (obj: any) => void) {
@@ -19,39 +19,39 @@ function hasScrollContent(node: any, func: (obj: any) => void) {
 
 }
 
-export default function Dom(props: IInput) {
+export default function Dom(props: IDom) {
     const {style, html} = props;
     const oldY = useRef(0);
-    const input = useRef<any>(null)
+    const dom = useRef<any>(null)
 
     useEffect(() => {
-        input.current = document.createElement('div');
-        input.current.id = 'div-' + Date.now()
-        input.current.innerHTML = html
-        input.current.style.pointerEvents = 'none'
-        input.current.style.position = "absolute";
+        dom.current = document.createElement('div');
+        dom.current.id = 'div-' + Date.now()
+        dom.current.innerHTML = html
+        dom.current.style.pointerEvents = 'none'
+        dom.current.style.position = "absolute";
         const app: any = document.querySelector('#canvas')
 
-        app.appendChild(input.current)
+        app.appendChild(dom.current)
 
         return () => {
-            app.removeChild(input.current)
-            input.current = null
+            app.removeChild(dom.current)
+            dom.current = null
         }
     }, [])
 
 
     const _customDrawer = (canvas: RevasCanvas, node: Node) => {
         //有动画会一直执行，需要做新旧数据判断处理
-        if (input?.current) {
+        if (dom?.current) {
             if (node?.frame?.y !== oldY?.current) {
                 oldY.current = node.frame.y
-                input.current.style.top = node.frame.y  + "px"
-                input.current.style.left = node.frame.x  + "px"
+                dom.current.style.top = node.frame.y  + "px"
+                dom.current.style.left = node.frame.x  + "px"
             }
             hasScrollContent(node, (scroll) => {
                 if(scroll){
-                    input.current.style.transform = `translate3d(0,-${scroll.scrollY}px,0)`
+                    dom.current.style.transform = `translate3d(0,-${scroll.scrollY}px,0)`
                 }
             })
         }
