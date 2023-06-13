b819321784812738d9e5fca904708edd831563af
lll <2222772@163.com>
2023-06-13 12:11:41 +0800

    feat(all): 优化代码

--- src/develop/App.tsx
+++ src/develop/App.tsx
@@ -16,25 +16,7 @@ import logo from './Intro/logo.png';
 import Top from './top';
 
 import SimpleRouter from './common/simple-router';
-// function randomInteger(max: number) {
-//     return Math.floor(Math.random() * (max + 1));
-// }
-// function randomRgbColor() {
-//     let r = randomInteger(255);
-//     let g = randomInteger(255);
-//     let b = randomInteger(255);
-//     return [r, g, b];
-// }
 
-// function randomHexColor() {
-//     let [r, g, b] = randomRgbColor();
-//
-//     let hr = r.toString(16).padStart(2, '0');
-//     let hg = g.toString(16).padStart(2, '0');
-//     let hb = b.toString(16).padStart(2, '0');
-//
-//     return '#' + hr + hg + hb;
-// }
 
 @withContext
 export default class App extends React.Component {
